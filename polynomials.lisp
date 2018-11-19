365d14941bc5df9095a2ab154530c7052d8a1a9b
szwedor <borkowicz.bartosz@gmail.com>
2018-11-19 22:05:02 +0100

    mul

--- polynomials.lisp
+++ polynomials.lisp
@@ -19,6 +19,47 @@
            (cons term1 (add-poly (rest poly1) poly2)))
           ((< (findpower term1) (findpower term2))
            (cons term2 (add-poly poly1 (rest poly2)))))))
+(defun power-poly(poly term)
+    (let ((power  (findpower  (car poly))) 
+          (factor (findfactor (car poly)))
+          (f (findfactor term))
+          (pow (findpower term)))
+    (cond ((null (rest poly))   (list (list (* f factor) (+ power pow))))
+    (T 
+    (cons (list (* f factor) (+ power pow)) (power-poly (rest poly) term)))
+    )
+    ))
+
+(defun mul-poly(poly1 poly2)
+    (let ((term (car poly2)))
+    (cond
+        ((null (rest poly2))  (power-poly poly1 term))
+        (T
+        
+   (add-poly (power-poly poly1 term) (mul-poly poly1 (rest poly2)))
+  ;  (mul-poly poly1 (rest poly2))
+    ))
+    )
+)
+;;(print (power-poly '((3 3)(2 2)(1 1)(4 4)) '(5 2)))
+ 
+;(print (power-poly '((3 5)) '(1 2)))
+ http://matematyka.pisz.pl/strona/1434.html
+(print (mul-poly '((4 5)(-6 2)(3 1)) '((5 4)(-2 2)(3 0))))
+ 
+;;(print (mul-poly '((4 5)) '((1 1))))
+
 
 ;;sample recursive execution
-;;(add-poly `((3 3)(2 2)(1 1)) (add-poly `((1 5)) `((2 5))))
+;print  (add-poly `((3 3)(2 2)(1 1)) `((3 3)(2 2)(1 1)) ))
+
+;;(print (add-poly `((1 5)) `((2 5))))
+(defmacro eight (a b) 
+  (add-poly a 
+    (add-poly a b)))
+
+;(print (eight '((1 5)) '((2 5))))
+;(add-poly `((1 5)) `((2 5))))
+
+;(print (macroexpand-1 '(eight ((1 5)) ((2 5)))))
+
