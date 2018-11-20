4b2d36d218b8cbac2c713ca749990feac93f7cd3
Bartłomiej Teodorczuk <deremaki@hotmail.com>
2018-11-20 01:47:17 +0100

    post merge clean up

--- polynomials.lisp
+++ polynomials.lisp
@@ -40,30 +40,7 @@
            (cons term1 (add-poly (rest poly1) poly2)))
           ((< (findpower term1) (findpower term2))
            (cons term2 (add-poly poly1 (rest poly2)))))))
-(defun power-poly(poly term)
-    (let ((power  (findpower  (car poly))) 
-          (factor (findfactor (car poly)))
-          (f (findfactor term))
-          (pow (findpower term)))
-    (cond ((null (rest poly))   (list (list (* f factor) (+ power pow))))
-    (T 
-    (cons (list (* f factor) (+ power pow)) (power-poly (rest poly) term)))
-    )
-    ))
 
-;;samples of recursive execution
-;;(add-poly `((3 3)(2 2)(1 1)) (add-poly `((1 5)) `((2 5))))
-;;(add-poly `((3 3)(2 2)(1 1)) (subtract-poly `((1 5)) `((2 5)(-2 2))))(defun mul-poly(poly1 poly2)
-    (let ((term (car poly2)))
-    (cond
-        ((null (rest poly2))  (power-poly poly1 term))
-        (T
-        
-   (add-poly (power-poly poly1 term) (mul-poly poly1 (rest poly2)))
-  ;  (mul-poly poly1 (rest poly2))
-    ))
-    )
-)
 (defun subtract-poly (poly1 poly2)
   (let ((term1 (car poly1))
         (term2 (car poly2)))
@@ -77,6 +54,28 @@
           ((< (findpower term1) (findpower term2))
            (cons term2 (subtract-poly poly1 (rest poly2)))))))
 
+(defun power-poly(poly term)
+    (let ((power  (findpower  (car poly))) 
+          (factor (findfactor (car poly)))
+          (f (findfactor term))
+          (pow (findpower term)))
+    (cond ((null (rest poly))   (list (list (* f factor) (+ power pow))))
+    (T 
+    (cons (list (* f factor) (+ power pow)) (power-poly (rest poly) term)))
+    )
+    ))
+
+ (defun mul-poly(poly1 poly2)
+    (let ((term (car poly2)))
+    (cond
+        ((null (rest poly2))  (power-poly poly1 term))
+        (T   
+   (add-poly (power-poly poly1 term) (mul-poly poly1 (rest poly2)))
+  ;  (mul-poly poly1 (rest poly2))
+    ))
+    )
+)
+
 (defun diff-poly(poly)
     (let ((power  (findpower  (car poly))) 
           (factor (findfactor (car poly))))
@@ -85,6 +84,7 @@
     (cons (list (* power factor) (- power 1)) (diff-poly (rest poly))))
     )
     ))
+
 (defun integral-poly(poly)
     (let ((power  (findpower  (car poly))) 
           (factor (findfactor (car poly))))
@@ -101,7 +101,7 @@
 ;;(print (power-poly '((3 3)(2 2)(1 1)(4 4)) '(5 2)))
  
 ;(print (power-poly '((3 5)) '(1 2)))
- http://matematyka.pisz.pl/strona/1434.html
+; http://matematyka.pisz.pl/strona/1434.html
 (print (mul-poly '((4 5)(-6 2)(3 1)) '((5 4)(-2 2)(3 0))))
  
 ;;(print (mul-poly '((4 5)) '((1 1))))
@@ -119,3 +119,8 @@
 ;(add-poly `((1 5)) `((2 5))))
 
 ;(print (macroexpand-1 '(eight ((1 5)) ((2 5)))))
+
+
+;;samples of recursive execution
+;;(add-poly `((3 3)(2 2)(1 1)) (add-poly `((1 5)) `((2 5))))
+;;(add-poly `((3 3)(2 2)(1 1)) (subtract-poly `((1 5)) `((2 5)(-2 2))))(defun mul-poly(poly1 poly2)
